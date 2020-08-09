95d6abc2d9b94e44fbc8bd36460aef169d1d0ad9
bedinuratascan <bedinuratascan@gmail.com>
2020-08-09 20:31:03 +0300

    Set up Asp.Net Core Identity

--- membershipSystem/Startup.cs
+++ membershipSystem/Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using membershipSystem.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +16,21 @@ namespace membershipSystem
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionString:DefaultConnectionString"]);
+            });
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
             services.AddMvc(option => option.EnableEndpointRouting = false);
 
         }
@@ -27,6 +42,7 @@ namespace membershipSystem
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
 
             //if (env.IsDevelopment())
             //{
