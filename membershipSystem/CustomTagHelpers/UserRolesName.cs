3aa8b203c64992f1d9360726e335d97c15fc9481
bedinuratascan <bedinuratascan@gmail.com>
2020-09-29 16:01:54 +0300

    Displaying roles assigned to users

---
+++ membershipSystem/CustomTagHelpers/UserRolesName.cs
@@ -0,0 +1,40 @@
+﻿using membershipSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace membershipSystem.CustomTagHelpers
+{
+    [HtmlTargetElement("td",Attributes ="user-roles")]
+    public class UserRolesName:TagHelper
+    {
+        public UserManager<AppUser> _userManager { get; set; }
+        public UserRolesName(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HtmlAttributeName("user-roles")]
+        public string UserId { get; set; } 
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            AppUser user = await _userManager.FindByIdAsync(UserId);
+
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+
+            string html = string.Empty;
+
+            roles.ToList().ForEach(x =>
+            {
+                html += $"<span class='badge badge-info'>{x}</span>";
+            });
+
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
